4d68e7ccef7869f12f55693fc6ab586f74fd868b
turumbekov bakir <bakirturumbekov34@gmail.com>
2023-11-18 01:08:40 +0600

    fix view home

--- service/catalog/views.py
+++ service/catalog/views.py
@@ -8,40 +8,14 @@ from .tasks import task_update_autoru_catalog
 
 @csrf_exempt
 def home(request):
-    """
-    Представление для главной страницы, отображающей список марок автомобилей и моделей.
-
-     Данный представитель обрабатывает запросы методом POST и GET.
-        - При методе POST извлекается выбранный идентификатор марки, и затем отображаются все связанные с ней модели.
-        - При методе GET отображаются все модели с использованием пагинации по 10 моделей на странице.
-
-    Это представление декорировано `csrf_exempt` для отключения защиты от CSRF.
-
-    Параметры:
-        request (HttpRequest): Объект HTTP-запроса.
-
-    Возвращает:
-        HttpResponse: Отображает шаблон 'home.html' со списком марок автомобилей или моделей.
-    """
     if request.method == 'POST':
         mark_id = request.POST.get('mark')
         selected_mark = Mark.objects.get(pk=mark_id)
         models = Model.objects.filter(mark=selected_mark)
         return render(request, 'home.html', {'models': models})
 
-    # Извлекаем все модели
-    all_models = Model.objects.all()
-
-    # Создаем объект пагинатора с количеством моделей на странице равным 10
-    paginator = Paginator(all_models, 10)
-
-    # Получаем номер текущей страницы из параметров запроса
-    page_number = request.GET.get('page')
-
-    # Получаем объект текущей страницы
-    page_obj = paginator.get_page(page_number)
-
-    return render(request, 'home.html', {'page_obj': page_obj})
+    marks = Mark.objects.all()
+    return render(request, 'home.html', {'marks': marks})
 
 
 @csrf_exempt
